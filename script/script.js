072f12754ffb98da516abca5d10aa49125c23edd
Mathis Thomsen <matthomster@gmail.com>
2018-05-26 11:28:13 +0200

    tweeks & Datenschutz

--- script/script.js
+++ script/script.js
@@ -18,7 +18,7 @@ $(document).ready(function(){
     function labnolIframe() {
         $('.spinner').show();
         var iframe = document.createElement("iframe");
-        iframe.setAttribute("src", "https://www.youtube.com/embed/" + this.parentNode.dataset.id + "?autoplay=1&autohide=2&border=0&wmode=opaque&enablejsapi=1&controls=1&showinfo=0");
+        iframe.setAttribute("src", "https://www.youtube-nocookie.com/embed/" + this.parentNode.dataset.id + "?autoplay=1&autohide=2&border=0&wmode=opaque&enablejsapi=1&controls=1&showinfo=0");
         iframe.setAttribute("frameborder", "0");
         iframe.setAttribute("id", "youtube-iframe");
         this.parentNode.replaceChild(iframe, this);
